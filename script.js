2570e8c0e33ae132a4e4e0ccc4c61a03661131b3
ManjariU <manjari1303@gmail.com>
2021-08-15 00:23:01 +0530

    Coding Challenge 01

---
+++ script.js
@@ -0,0 +1,46 @@
+//METHOD 01
+
+//test case 01
+// let marksMass = 78;
+// let johnsMass = 92;
+// let marksHeight = 1.69;
+// let johnsHeight = 1.95;
+
+//test case 02
+// const marksMass = 95;
+// const johnsMass = 85;
+// const marksHeight = 1.95;
+// const johnsHeight = 1.76;
+
+// const marksBMI = marksMass / marksHeight ** 2;
+// const johnsBMI = johnsMass / johnsHeight ** 2;
+// const markHigherBMI = marksBMI > johnsBMI;
+
+// console.log(marksBMI, johnsBMI, markHigherBMI);
+
+
+//METHOD 02
+
+//case 01:
+let marksMass = 78;
+let johnsMass = 92;
+let marksHeight = 1.69;
+let johnsHeight = 1.95;
+
+let marksBMI = marksMass / marksHeight ** 2;
+let johnsBMI = johnsMass / johnsHeight ** 2;
+let markHigherBMI = marksBMI > johnsBMI;
+
+console.log(marksBMI, johnsBMI, markHigherBMI);
+
+//case 02:
+marksMass = 95;
+johnsMass = 85;
+marksHeight = 1.95;
+johnsHeight = 1.76;
+
+marksBMI = marksMass / marksHeight ** 2;
+johnsBMI = johnsMass / johnsHeight ** 2;
+markHigherBMI = marksBMI > johnsBMI;
+
+console.log(marksBMI, johnsBMI, markHigherBMI);
